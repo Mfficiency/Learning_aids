a540dcacc68564da89bc7e4124da659cb0cbbde8
mfficiency <63962741+Mfficiency@users.noreply.github.com>
2025-12-01 22:05:53 +0100

    kind of white

--- light_saber/compare.py
+++ light_saber/compare.py
@@ -1,42 +1,35 @@
 strip = neopixel.create(DigitalPin.P0, 53, NeoPixelMode.RGBW)
-BRIGHTNESS = 255
+
+BRIGHTNESS = 10
 def apply_brightness(r, g, b, w=0):
     r = r * BRIGHTNESS // 255
     g = g * BRIGHTNESS // 255
     b = b * BRIGHTNESS // 255
-    w = w * BRIGHTNESS // 255
-    return (r << 16) | (g << 8) | b | (w << 24)
-pulsing = False
-def pulse_cyan():
-    global pulsing
-    pulsing = True
-    while pulsing:
-        # ramp up
-        for i in range(0, 256, 5):
-            if not pulsing:
-                return
-            color = apply_brightness(0, i, 255)
-            strip.show_color(color)
-            basic.pause(20)
-        # ramp down
-        for i in range(255, -1, -5):
-            if not pulsing:
-                return
-            color = apply_brightness(0, i, 255)
-            strip.show_color(color)
-            basic.pause(20)
-def show_white():
-    r = 0 
-    g = 0 
-    b = 0 
-    # Ramp up white slowly
-    for w in range(0, 256, 5):
-        color = apply_brightness(r, g, b, w)
+    return (r << 16) | (g << 8) | b
+
+def pulse_blue():
+    for i in range(0, 256, 5):
+        color = apply_brightness(0, 0, i)
         strip.show_color(color)
-        basic.pause(20)
+        basic.pause(30)
+    for i in range(255, -1, -5):
+        color = apply_brightness(0, 0, i)
+        strip.show_color(color)
+        basic.pause(30)
+
+def show_white():
+    # RGBW strip, so set W channel to brightness
+    r = 255
+    g = 255
+    b = 255
+    w = 255
+    strip.show_color(apply_brightness(r, g, b, w))
+
 def on_button_a():
-    pulse_cyan()
+    pulse_blue()
+
 def on_button_b():
     show_white()
+
 input.on_button_pressed(Button.A, on_button_a)
 input.on_button_pressed(Button.B, on_button_b)
